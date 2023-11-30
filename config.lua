f95c8d703d179c0787ed6a9cba1435fe69ae9566
huh <itzcbuchbergmc@gmail.com>
2023-11-30 19:52:11 +0100

    config ændring

--- config.lua
+++ config.lua
@@ -8,3 +8,29 @@ Config.PEDModel = 'a_f_y_femaleagent'
 Config.FrontTarget = vec3(-1570.7935, -574.9966, 108.5228)
 
 Config.AdvokatJob = ''
+
+
+Config.Elevators = {
+    Advokat = { -- Elevator name(Doesn't show it's just to name table)
+        [1] = {
+            coords = vec3(-1579.6760, -561.1122, 108.5229),
+            heading = 300.8177,
+            title = 'Advokat Kontor', 
+            description = 'Tilgå advokat kontoret',
+            target = {
+                width = 5,
+                length = 4
+            }
+        },
+        [2] = {
+            coords = vec3(-1581.3550, -558.2264, 34.9531),
+            heading = 199.6223,
+            title = 'Udgang',
+            description = 'Tilgå udgangen',
+            target = {
+            width = 5,
+            length = 4
+            }
+        },
+    },
+}
