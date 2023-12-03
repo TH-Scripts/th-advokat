281da7028a6a95fe149400a67750dd1ed0a48c83
thegamer5095 <klintandersenjonas@gmail.com>
2023-12-03 12:10:20 +0100

    Error handling

--- client/functions.lua
+++ client/functions.lua
@@ -11,6 +11,13 @@ function CreateCase()
         {type = 'date', label = 'Angiv dato', icon = 'calendar-days', format = 'DD/MM/YYYY', default = true, disabled = true, returnString = true},
     })
 
+    if input == nil then
+        lib.notify({
+            description = 'Du annullerede oprettelse af sagen',
+            type = 'inform'
+        })
+    end
+
     TriggerServerEvent('th-advokat:CreateCase', input[1], input[2], input[3], input[4], input[5])
 end
 
